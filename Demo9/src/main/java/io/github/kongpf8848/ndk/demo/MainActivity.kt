6cfa367fdf4f2f2a137bbf688039d71810157aba
kongpf8848 <kongpf8848@gmail.com>
2025-04-25 11:20:35 +0800

    update

--- Demo9/src/main/java/io/github/kongpf8848/ndk/demo/MainActivity.kt
+++ Demo9/src/main/java/io/github/kongpf8848/ndk/demo/MainActivity.kt
@@ -1,11 +1,8 @@
 package io.github.kongpf8848.ndk.demo
 
-import android.content.Context
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import io.github.kongpf8848.ndk.demo.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,38 +10,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button1: Button = findViewById(R.id.button1)
-        val button2: Button = findViewById(R.id.button2)
-        val button3: Button = findViewById(R.id.button3)
+
 
         button1.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                jniGetPackageName(applicationContext),
-                Toast.LENGTH_SHORT
-            ).show()
+           testCrash()
         }
 
-        button2.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                jniGetSignature(applicationContext),
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-
-        button3.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                Util.getAppSignature(applicationContext),
-                Toast.LENGTH_SHORT
-            ).show()
-        }
 
     }
 
-    private external fun jniGetPackageName(context: Context): String
-    private external fun jniGetSignature(context: Context): String
-
+    private external fun testCrash(): Unit
 
     companion object {
         init {
