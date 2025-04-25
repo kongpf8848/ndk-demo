e89808729c9b4e160332d2d900e66fc094c1b4ea
kongpf8848 <kongpf8848@gmail.com>
2025-04-25 20:58:09 +0800

    add breakpad

--- Demo9/src/main/cpp/native-lib.cpp
+++ Demo9/src/main/cpp/native-lib.cpp
@@ -9,13 +9,37 @@
 #define LOGW(...)  __android_log_print(ANDROID_LOG_WARN,TAG,__VA_ARGS__)
 #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,TAG,__VA_ARGS__)
 
+#include <stdio.h>
+#include "breakpad/src/client/linux/handler/exception_handler.h"
+#include "breakpad/src/client/linux/handler/minidump_descriptor.h"
+
+bool DumpCallback(const google_breakpad::MinidumpDescriptor &descriptor,
+                  void *context,
+                  bool succeeded) {
+    LOGD("DumpPath: %s\n", descriptor.path());
+    return succeeded;
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_io_github_kongpf8848_ndk_demo_MainActivity_initBreakpad(
+        JNIEnv *env,
+        jobject clazz, jstring path) {
+    LOGD("initBreakpad11");
+    const char *dump_path = env->GetStringUTFChars(path, nullptr);
+
+    google_breakpad::MinidumpDescriptor descriptor(dump_path);
+    google_breakpad::ExceptionHandler eh(descriptor, nullptr, DumpCallback,
+                                         nullptr, true, -1);
+    env->ReleaseStringUTFChars(path, dump_path);
+    LOGD("initBreakpad22");
+}
 
 extern "C" JNIEXPORT void JNICALL
 Java_io_github_kongpf8848_ndk_demo_MainActivity_testCrash(
         JNIEnv *env,
         jobject clazz) {
     LOGD("testCrash");
-    int *a = NULL;
+    volatile int *a = nullptr;
     *a = 1;
 }
 
--- Demo9/src/main/java/io/github/kongpf8848/ndk/demo/MainActivity.kt
+++ Demo9/src/main/java/io/github/kongpf8848/ndk/demo/MainActivity.kt
@@ -1,8 +1,13 @@
 package io.github.kongpf8848.ndk.demo
 
 import android.os.Bundle
+import android.os.Looper
+import android.os.MessageQueue
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,16 +15,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button1: Button = findViewById(R.id.button1)
-
-
         button1.setOnClickListener {
-           testCrash()
+            testCrash()
+        }
+        Looper.myQueue().addIdleHandler {
+            val file = File(applicationContext.externalCacheDir, "native_crash")
+            if (!file.exists()) {
+                file.mkdirs()
+            }
+            initBreakpad(file.absolutePath)
+            false
         }
 
 
     }
 
-    private external fun testCrash(): Unit
+    private external fun initBreakpad(path: String)
+    private external fun testCrash()
 
     companion object {
         init {
