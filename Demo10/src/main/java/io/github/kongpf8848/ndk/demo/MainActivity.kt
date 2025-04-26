a4d7ecbdca8a8a770ceaa75cc6bfd5240b1f0775
kongpf8848 <kongpf8848@gmail.com>
2025-04-26 21:16:10 +0800

    add breakpad

--- Demo10/src/main/java/io/github/kongpf8848/ndk/demo/MainActivity.kt
+++ Demo10/src/main/java/io/github/kongpf8848/ndk/demo/MainActivity.kt
@@ -1,11 +1,15 @@
 package io.github.kongpf8848.ndk.demo
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Looper
 import android.os.MessageQueue
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import java.io.File
 
 
@@ -16,18 +20,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val button1: Button = findViewById(R.id.button1)
         button1.setOnClickListener {
+            initPath()
             testCrash()
         }
-        Looper.myQueue().addIdleHandler {
-            val file = File(applicationContext.externalCacheDir, "native_crash")
-            if (!file.exists()) {
-                file.mkdirs()
-            }
-            initBreakpad(file.absolutePath)
-            false
+        if (ActivityCompat.checkSelfPermission(this@MainActivity,Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                100
+            )
         }
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==100 && grantResults[0]== Activity.RESULT_OK) {
+            initPath()
+        }
+    }
 
+    private fun initPath(){
+        val file = File(applicationContext.externalCacheDir, "crash")
+        if (!file.exists()) {
+            file.mkdirs()
+        }
+        initBreakpad(file.absolutePath)
     }
 
     private external fun initBreakpad(path: String)
@@ -35,6 +56,7 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         init {
+            System.loadLibrary("breakpad")
             System.loadLibrary("native-lib")
         }
     }
